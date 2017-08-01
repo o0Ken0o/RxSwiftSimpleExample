fc7216d672e1f5cf8ecb5ca73af7b3db298cd676
Ken Siu <kensiu20042000@gmail.com>
2017-08-01 14:56:24 +0800

    initial commit

---
+++ SimpleExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleExample
+//
+//  Created by Kam Hei Siu on 1/8/2017.
+//  Copyright © 2017 Kam Hei Siu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
