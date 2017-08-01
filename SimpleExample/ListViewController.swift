44d3cc1146bbcfc96f707f52ee516ea20332597b
Ken Siu <kensiu20042000@gmail.com>
2017-08-01 17:41:36 +0800

    make the tableView reactive using RxCocoa and RxSwift

--- SimpleExample/ListViewController.swift
+++ SimpleExample/ListViewController.swift
@@ -7,22 +7,54 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ListViewController: UIViewController {
 
     @IBOutlet var tableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
     
+    private let disposeBag = DisposeBag()
+    
     let cellIdentifier = "CellIdentifier"
+    var data = Observable.just(names)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupTableView()
+        setupCells()
+        setupCellTapping()
     }
     
     private func setupTableView() {
         tableView.backgroundColor = UIColor.Custom.orange
+    }
+    
+    private func setupCells() {
+        data.bind(to: tableView
+            .rx
+            .items(cellIdentifier: cellIdentifier, cellType: UITableViewCell.self)) {
+            row, name, cell in
+            cell.textLabel?.text = name
+        }.addDisposableTo(disposeBag)
+    }
+    
+    private func setupCellTapping() {
+        tableView
+            .rx
+            .modelSelected(String.self)
+            .subscribe(onNext: {
+                [unowned self] name in
+                
+                if let indexPath = self.tableView.indexPathForSelectedRow, let selectedCell = self.tableView.cellForRow(at: indexPath) {
+                    selectedCell.contentView.backgroundColor = .orange
+                    self.tableView.deselectRow(at: indexPath, animated: true)
+                }
+                
+            })
+            .addDisposableTo(disposeBag)
     }
 
 }
---
+++ SimpleExample/Seedings.swift
@@ -0,0 +1,11 @@
+//
+//  Seedings.swift
+//  SimpleExample
+//
+//  Created by Kam Hei Siu on 1/8/2017.
+//  Copyright © 2017 Kam Hei Siu. All rights reserved.
+//
+
+import Foundation
+
+var names = ["Barry Allen", "Joe West", "Iris West", "Wally West", "Oliver Queen", "Michael Scotfield"]
